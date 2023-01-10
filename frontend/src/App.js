5ee74fc9a3687e3ce8f292ec3f850a4314dc25c5
Elvis Yung <elvisckyung@gmail.com>
2023-01-10 02:07:09 +0000

    Router v6 Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homescreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Homescreen />
+          <Routes>
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/' element={<Homescreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
