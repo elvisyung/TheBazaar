f5adab6aca258fc070a297a6c95bb0a36d5f6a08
Elvis Yung <elvisckyung@gmail.com>
2023-02-01 23:36:42 +0000

    Fix 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const source = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(source, {
       useNewUrlParser: true,
     });
 
