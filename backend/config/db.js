ae6c37cdc8237389b17992af0ffd2b19e97ad711
Elvis Yung <elvisckyung@gmail.com>
2023-02-01 23:15:49 +0000

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
