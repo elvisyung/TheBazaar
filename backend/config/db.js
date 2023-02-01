8ea83b5bf7d9f500d2b6c6272284e243f83d1780
Elvis Yung <elvisckyung@gmail.com>
2023-02-01 22:03:22 +0000

    Mongoose Error

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(
+      'mongodb+srv://elvis1234:Wolfboy99@thebazaar.8f8f2by.mongodb.net/thebazaar?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
